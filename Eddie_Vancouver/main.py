e7853577a507d021d1ee4a1a2465ad18d6641338
Eddie <kp1472@gmail.com>
2025-04-08 17:17:51 -0700

    웹 기반이 아닌 CLI 버전

--- Eddie_Vancouver/main.py
+++ Eddie_Vancouver/main.py
@@ -1,35 +1,39 @@
-import streamlit as st
 from Utils.Calculator import calculate_expense
 from Utils.Visualizer import plot_pie_chart
+import argparse
 
-st.set_page_config(page_title="밴쿠버 생활비 계산기", layout="centered")
 
-st.title("💸 나만의 밴쿠버 생활비 계산기")
-st.write("당신의 월 지출을 입력하면 연간 지출과 비율을 계산해드립니다!")
+def main():
+    print("======= 생활비 계산기 =======")
+    categories = {}
 
-# 사용자 입력
-rent = st.number_input("🏠 주거비 (렌트)", min_value=0, step=10)
-food = st.number_input("🍽 식비", min_value=0, step=10)
-transport = st.number_input("🚌 교통비", min_value=0, step=10)
-phone = st.number_input("📱 통신비", min_value=0, step=10)
-leisure = st.number_input("🎮 여가/문화", min_value=0, step=10)
-shopping = st.number_input("🛍 쇼핑/개인 소비", min_value=0, step=10)
-etc = st.number_input("💼 기타", min_value=0, step=10)
+    # 항목 입력
+    categories['월세'] = float(input("월세 지출 (CAD): "))
+    categories['식비'] = float(input("식비 지출 (CAD): "))
+    categories['교통비'] = float(input("교통비 지출 (CAD): "))
+    categories['기타'] = float(input("기타 지출 (CAD): "))
 
-if st.button("지출 계산하기"):
-    categories = {
-        "주거비": rent,
-        "식비": food,
-        "교통비": transport,
-        "통신비": phone,
-        "여가/문화": leisure,
-        "쇼핑": shopping,
-        "기타": etc
-    }
-
+    # 계산
     total_monthly, total_yearly = calculate_expense(categories)
-    st.subheader("📊 결과")
-    st.write(f"**월 지출 총합:** ${total_monthly:,.2f}")
-    st.write(f"**연 지출 총합:** ${total_yearly:,.2f}")
+    print(f"\n총 월 지출: ${total_monthly}")
+    print(f"총 연 지출: ${total_yearly}")
 
-    st.plotly_chart(plot_pie_chart(categories))
+
+
+    # 시각화 저장 시 저장 유무 출력 체크 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save', action = 'store_true', help='이미지 저장 여부')
+    args = parser.parse_args()
+
+
+    if args.save:
+        # 시각화
+        fig = plot_pie_chart(categories)
+        print("차트 이미지 저장 중......(잠시만 기다려 주세요!)")
+        fig.write_image("pie_chart.png")  # 이미지로 저장
+        print("\n[차트] pie_chart.png 로 저장 완료!")
+    else:
+        print("\n(이미지 저장 없이 종료됩니다.)")
+
+if __name__ == "__main__":
+    main()
